4f77260e73378796fab81763fd02fbc03da7ec48
jonatasmarruda-prog <jonatasmarruda@gmail.com>
2026-09-02 14:31:49 -0400

    Adicionar service worker para uso offline

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='inspecao-incendio-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match('./index.html'))));});
