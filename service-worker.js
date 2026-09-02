490c3454b644c53a1ec20341fd344df9e90fcf47
jonatasmarruda-prog <jonatasmarruda@gmail.com>
2026-09-02 14:40:46 -0400

    Forcar atualização do cache PWA para versão 3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='inspecao-incendio-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='inspecao-incendio-v3';
+const ASSETS=['./','./index.html','./manifest.json','./service-worker.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
